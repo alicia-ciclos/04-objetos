b5f24e47c0c55db497ab16887b2590e1c1f952d1
ALICE WYAN GARCIA MARTIN <alicew.garmar@educa.jcyl.es>
2024-11-26 14:11:50 +0100

    Clase 26-11-2024

--- app.js
+++ app.js
@@ -44,6 +44,83 @@ class Empleado {
     }
 }
 
+function renderTable() {
+    let tabla = document.getElementById("lista-empleados")
+    empleados.forEach(empleado => {
+        let fila = empleado.render();
+        tabla.appendChild(fila);
+    });
+}
+
+function addEventListeners() {
+    let boton = document.getElementById("formulario-enviar");
+    boton.addEventListener('click', evento => {
+        evento.preventDefault();
+    
+        let nombre = document.getElementById('nombre').value
+        let apellido = document.getElementById('apellido').value
+        let nacimiento = document.getElementById('nacimiento').value
+        let sueldo = document.getElementById('sueldo').value
+    
+        let empleado = new Empleado(nombre, apellido, nacimiento, sueldo)
+        empleados.push(empleado)
+    
+        let tabla = document.getElementById("lista-empleados")
+        let fila = empleado.render()
+        tabla.appendChild(fila)
+    });
+}
+
+function crearDiv(clase) {
+    let fila = document.createElement('div');
+    fila.setAttribute('class', clase);
+    return fila;
+}
+
+function filaFormulario(id, titulo) {
+    let fila = crearDiv('formulario-fila');
+
+    // <label for="nacimiento">Nacimiento</label>
+    let label = document.createElement('label');
+    label.setAttribute('for', id);
+    label.textContent = titulo;
+    fila.appendChild(label);
+
+    // <input type="text" name="nacimiento" id="nacimiento">
+    let input = document.createElement('input');
+    input.setAttribute('type', 'text');
+    input.setAttribute('name', id);
+    input.setAttribute('id', id);
+    fila.appendChild(input);
+
+    return fila;
+}
+
+function renderForm() {
+    let form = document.getElementById('formulario');
+
+    let formularioContenido = crearDiv('formulario-contenido');
+
+    formularioContenido.appendChild(filaFormulario('nombre', 'Nombre'));
+    formularioContenido.appendChild(filaFormulario('apellido', 'Apellido'));
+    formularioContenido.appendChild(filaFormulario('nacimiento', 'Año de nacimiento'));
+    formularioContenido.appendChild(filaFormulario('sueldo', 'Sueldo'));
+    
+    formularioContenido.appendChild(crearDiv('formulario-fila'));
+
+    let divBoton = crearDiv('formulario-fila');
+
+    let boton = document.createElement('button');
+    boton.setAttribute('id', 'formulario-enviar');
+    boton.textContent = 'Añadir';
+    divBoton.appendChild(boton);
+
+    formularioContenido.appendChild(divBoton);
+
+    form.appendChild(formularioContenido);
+}
+
+
 let empleados = [
     new Empleado("Paco", "Fiestas", 1997, 33000),
     new Empleado("Chindas", "Vinto", 2001, 27000),
@@ -54,25 +131,10 @@ let empleados = [
     new Empleado("Eulalio", "Fernandez", 1999, 54000),
 ]
 
-let tabla = document.getElementById("lista-empleados")
-empleados.forEach(empleado => {
-    let fila = empleado.render();
-    tabla.appendChild(fila);
-});
+function main() {
+    renderTable();
+    renderForm();
+    addEventListeners();
+};
 
-let boton = document.getElementById("formulario-enviar");
-boton.addEventListener('click', evento => {
-    evento.preventDefault();
-
-    let nombre = document.getElementById('nombre').value
-    let apellido = document.getElementById('apellido').value
-    let nacimiento = document.getElementById('nacimiento').value
-    let sueldo = document.getElementById('sueldo').value
-
-    let empleado = new Empleado(nombre, apellido, nacimiento, sueldo)
-    empleados.push(empleado)
-
-    let tabla = document.getElementById("lista-empleados")
-    let fila = empleado.render()
-    tabla.appendChild(fila)
-});
+document.addEventListener('DOMContentLoaded', main);
